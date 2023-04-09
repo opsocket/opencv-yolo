8aad293c282f091351d0603cd9604272a45f2df7
opsocket <opsocket@pm.me>
2023-04-09 13:23:55 -0400

    :memo: license reference

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+"""
+Real-time Object Detection with OpenCV and YOLOv4-tiny 
+Copyright (C) 2020-2023 opsocket <opsocket@pm.me>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 import cv2
 import threading
 import numpy as np
