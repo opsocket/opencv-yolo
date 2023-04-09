4ec16b9c822b21f25ede4c07645789dbda022f4c
opsocket <opsocket@pm.me>
2023-04-09 13:10:43 -0400

    :sparkles: update to YOLOv4 and various improvements

--- README.md
+++ README.md
@@ -1,18 +1,11 @@
-## Real-time Object Detection with OpenCV and YOLOv3
+# Real-time Object Detection with OpenCV and YOLOv4-tiny
 
 ![Banana](banana.png)
 
-Read more at https://docs.opencv.org and https://pjreddie.com/darknet/yolo/
+Read more at https://docs.opencv.org and https://pjreddie.com/darknet/yolo
 
-#### Prerequisites
-
-Get configuration files and weights (i.e. YOLOv3-320)
-
-1. wget https://pjreddie.com/media/files/yolov3.weights
-2. wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg
-
-#### Start capture
+## Start capture
 
 1. `python -m venv env`
 2. `pip install -r requirements.txt`
-3. `python main.py`
+3. `python3 main.py`
--- main.py
+++ main.py
@@ -1,62 +1,83 @@
 import cv2
+import threading
 import numpy as np
 
-# Define a capture device
-cam = cv2.VideoCapture(0)
-
-# Load YOLOv3-320 (mAP: 51.5 / FPS: 45)
-net = cv2.dnn.readNet("yolov3.weights","yolov3.cfg")
-
 # Load COCO names
-names = []
-with open("coco.names","r") as f:
-	names = [ line.strip() for line in f.readlines() ]
+names = [ line.strip() for line in open("assets/coco/names","r").readlines() ]
 
+# Load the YOLOv4 model
+net = cv2.dnn.readNet('assets/yolov4-tiny/yolov4-tiny.weights', 'assets/yolov4-tiny/yolov4-tiny.cfg')
+
+# retrieve output layers identifiers
 layer_names = net.getLayerNames()
-outputlayers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-colors = np.random.uniform(0,255, size=(len(names), 3))
+outputlayers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 
-while(True):
-	# Capture frame-by-frame
-	ret, frame = cam.read()
-	# Our operations on the frame start here
-	height,width,channels = frame.shape
-	# Create blob from frame
-	blob = cv2.dnn.blobFromImage(frame,0.00392,(416,416),(0,0,0),True,crop=False)
-	# Set blob as net input source
-	net.setInput(blob)
-	# Runs forward pass
-	outs = net.forward(outputlayers)
-	# Filters results with low uncertainty
-	name_ids, confidences, boxes = [], [], []
-	for out in outs:
-		for detection in out:
-			scores = detection[5:]
-			name_id = np.argmax(scores)
-			confidence = scores[name_id]
-			if confidence > 0.5:
-				center_x, center_y = int(detection[0]*width), int(detection[1]*height) # Compute center position
-				w, h = int(detection[2]*width), int(detection[3]*height) # Compute dimensions
-				x, y = int(center_x - w/2), int(center_y - h/2) # Compute origin
-				boxes.append([x,y,w,h]) # Append object's box position
-				confidences.append(float(confidence)) # Append object's confidence percentage
-				name_ids.append(name_id) # Append object's class id
+# compute box colors
+colors = np.random.uniform(50,255, size=(len(names), 3))
 
-	# Performs non maximum suppression given boxes and corresponding scores to retrieve indexes
-	indexes = cv2.dnn.NMSBoxes(boxes,confidences,0.4,0.6)
-	# Append boxes, confidence and label on frame
-	for i in range(len(boxes)):
-		if i in indexes:
-			x,y,w,h = boxes[i]
-			label = str(names[name_ids[i]])
-			cv2.rectangle(frame,(x,y),(x+w,y+h),colors[i],2)
-			cv2.putText(frame,"%s %d %%" % (label, confidences[i]*100),(x,y-10),cv2.FONT_HERSHEY_PLAIN,1,(0,0,0),1)
+# open capture device
+cap = cv2.VideoCapture(0)
 
-	# Show frame
-	cv2.imshow("Detection", frame)
-	# Sleep for 1ms to process the draw requests from cv2.imshow()
-	cv2.waitKey(1)
+if not cap.isOpened():
+    print("Failed to initialize video capture object")
+    exit()
 
-# When everything done, release the capture and destroy all windows
-cam.release()
+# font parameters
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = .5
+thickness = 1
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # unpack frame's shape
+    height, width, channels = frame.shape 
+    
+    # resize frame to neural net input shape
+    blob = cv2.dnn.blobFromImage(frame, 1/255, (416, 416), (0,0,0), True, crop=False)
+    
+    # define neural net input
+    net.setInput(blob)
+    
+    # run forward pass and filter results with low confidence
+    outs = net.forward(outputlayers)
+    boxes, confidences, class_ids = [], [], []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > .25:
+                center_x, center_y, w, h = (detection[0:4] * np.array([width, height, width, height])).astype("int")
+                boxes.append([int(center_x - w/2), int(center_y - h/2), w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    # perform non-maximum suppression on boxes and corresponding scores to retrieve indexes
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.3, 0.2)
+
+    # draw boxes and labels on frame
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            name = names[class_ids[i]]
+            label = f"{name} {confidences[i] * 100:.2f}%"
+            color = colors[names.index(name)]           
+            # retrieve text size to compute background rectangle position 
+            text_size, _ = cv2.getTextSize(label, font, font_scale, thickness)
+            # draw box
+            cv2.rectangle(frame, (int(x), int(y)), (int(x+w), int(y+h)), color, 2)
+            # draw label background
+            cv2.rectangle(frame, (int(x-1), int(y)), (int(x+text_size[0]+10), int(y-text_size[1]-14)), color, -1)
+            # draw label text
+            cv2.putText(frame, label, (int(x+5), int(y-10)), font, font_scale, (0,0,0), thickness, cv2.LINE_AA, False)
+
+    # display the resulting frame
+    cv2.imshow('Object Detection', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
 cv2.destroyAllWindows()
